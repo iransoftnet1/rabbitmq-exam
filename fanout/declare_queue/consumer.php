8e2457fcd5a4598b0ff1badfe47620a53d4b44b4
darkMode <45430743+iransoftnet1@users.noreply.github.com>
2024-06-25 16:29:12 +0330

    feat(fanout): declare_queue

---
+++ fanout/declare_queue/consumer.php
@@ -0,0 +1,27 @@
+<?php
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+$connection = new AMQPStreamConnection('127.0.0.1', 5672, 'admin', '123456');
+$channel = $connection->channel();
+
+// create dynamic queue
+list($queueName, ,) = $channel->queue_declare('', false, false, false, false);
+$channel->queue_bind($queueName,'purgeDataFanout');
+
+// --
+
+$callback = function (AMQPMessage $message) use ($channel) {
+    echo $message->body . PHP_EOL;
+};
+
+$channel->basic_consume($queueName, '', false, true, false, false, $callback);
+
+while ($channel->getConnection()->isConnected()) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
---
+++ fanout/declare_queue/send.php
@@ -0,0 +1,17 @@
+<?php
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+$connection = new AMQPStreamConnection('127.0.0.1', 5672, 'admin', '123456');
+$channel = $connection->channel();
+
+$data = new AMQPMessage(json_encode(['key' => 'number#1']));
+
+$channel->basic_publish($data, 'purgeDataFanout');
+
+// ....
+
+$channel->close();
+$connection->close();
