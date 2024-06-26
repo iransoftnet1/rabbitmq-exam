ef2107b4a24bec6acc7f80b8851e64b95eac00e7
darkMode <45430743+iransoftnet1@users.noreply.github.com>
2024-06-26 19:23:04 +0330

    feat(direct.send): add properties in message

--- direct/send.php
+++ direct/send.php
@@ -7,7 +7,25 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $connection = new AMQPStreamConnection('127.0.0.1', 5672, 'admin', '123456');
 $channel = $connection->channel();
 
-$message = new AMQPMessage('Test Message');
+//$message = new AMQPMessage('Test Message');
+$message = new AMQPMessage('Test Message', [
+    // message properties
+    'content_type' => 'text/plain',
+    'content_encoding' => 'gzip',
+    'timestamp' => 10000000000,
+    'expiration' => (string)time(),
+    /**
+     * 1 => save on memory and remove while server restart
+     * 2 => save on disk (stable)
+     */
+    'delivery_mode' => 2,
+    'app_id' => '11',
+    'type' => 'exchange.queue',
+    'headers' => [
+        'foo' => 'bar',
+        'age' => 12
+    ]
+]);
 
 $channel->basic_publish($message, 'sendLogDirect', 'log');
 
